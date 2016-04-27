d56798abfced15173ec987c1a75141321ae81496
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-04-27 11:51:28 +0300

    Add property names as get/set only logs are hard to track

--- tracers/calltree.js
+++ tracers/calltree.js
@@ -3,6 +3,8 @@ var traceDepth = 0;
 var eventQueue = [];
 var nodes = {};
 
+var definePropertyNodes = {};
+
 var time = function() { return 0; };
 if (global.android) {
 	var sys = java.lang.System;
@@ -35,8 +37,34 @@ if (typeof __tracer !== 'undefined') {
 	
 	__tracer.add = function(info, data) {
 		if (data.nodes) {
+			var propertyDefinitions = [];
+			var gettersAndSetters = [];
 			data.nodes.forEach(function(node) {
 				nodes[node.id] = node;
+				if (node.name === "Object.defineProperty" && node.type === "callsite") {
+					var propDef = { node: node, get: undefined, set: undefined };
+					propertyDefinitions.push(propDef);
+					definePropertyNodes[node.id] = propDef;
+				} else if ((node.name === "get" || node.name === "set") && node.type === "function") {
+					gettersAndSetters.push(node);
+				}
+			});
+			gettersAndSetters.forEach(function(node) {
+				for (var i = 0; i < propertyDefinitions.length; i++) {
+					var propDef = propertyDefinitions[i];
+					var propDefNode = propDef.node;
+					// that contains get/set node
+					if ((propDefNode.start.line < node.start.line || (propDefNode.start.line == node.start.line && propDefNode.start.column <= node.start.column)) &&
+						(node.end.line < propDefNode.end.line || (node.end.line == propDefNode.end.line && node.end.column <= propDefNode.end.column))) {
+						// will set that node's name once called with propety name
+						if (node.name === "get") {
+							propDef.get = node.id;
+						} else {
+							propDef.set = node.id;
+						}
+						break;
+					}
+				}
 			});
 		}
 		return info;
@@ -47,7 +75,6 @@ if (typeof __tracer !== 'undefined') {
 	}
 	
 	__tracer.traceFileExit = function(info) {
-		//console.log("traceFileExit: " + info.nodeId);
 		return info;
 	}
 	
@@ -83,7 +110,28 @@ if (typeof __tracer !== 'undefined') {
 			fthis = info.this;
 			func = fthis[info.property];
 		}
-
+		
+		var isDefineProperty = info.this === Object && info.property === "defineProperty";
+		if (isDefineProperty) {
+			var nodeId = info.nodeId;
+			var definePropertyNode = definePropertyNodes[nodeId];
+			var getNode = nodes[definePropertyNode.get];
+			var setNode = nodes[definePropertyNode.set];
+			if (definePropertyNode) {
+				return function () {
+					var propertyName = arguments[1];
+					if (propertyName) {
+						if (getNode) {
+							getNode.name = "get " + propertyName;
+						}
+						if (setNode) {
+							setNode.name = "set " + propertyName;
+						}
+					}
+					return func.apply(customThis ? fthis : this, arguments);
+				};
+			}
+		}
 		return function () {
 			return func.apply(customThis ? fthis : this, arguments);
 		};
@@ -113,20 +161,25 @@ exports.stop = stop;
 function print() {
 	var indent = "";
 	var stack = [];
+	var offset = undefined;
 	for (var i = 0; i < eventQueue.length; i++) {
 		var trace = eventQueue[i];
 		var node = nodes[trace.nodeId];
 		
 		if (trace.type === "enter") {
+			if (!offset) {
+				offset = trace.time;
+				console.log("CALLTREE: // offset: " + trace.time);
+			}
 			stack.push(trace);
-			console.log("CALLTREE: " + indent + node.name + "() { // " + node.id);
+			console.log("CALLTREE: " + indent + node.name + "() { // " + prettyNodeSource(node));
 			indent += "  ";
-			console.log("CALLTREE: " + indent + "// start: " + trace.time);
+			console.log("CALLTREE: " + indent + "// start:    " + (trace.time - offset) + "ms");
 		}
 		
 		if (trace.type === "exit") {
 			if (stack.length <= 0) {
-				console.log("CALLTREE: // } missing start time for: " + node.name + " " + node.id);
+				console.log("CALLTREE: // } missing start time for: " + node.name + " " + prettyNodeSource(node));
 				continue;
 			}
 			var startTrace = stack.pop();
@@ -134,10 +187,10 @@ function print() {
 				console.log("CALLTREE: ERROR: Enter/Exit pairs do not match!");
 				break;
 			}
-			console.log("CALLTREE: " + indent  + "// end:   " + trace.time);
+			console.log("CALLTREE: " + indent  + "// end:      " + (trace.time - offset) + "ms");
 			indent = indent.substr(0, indent.length - 2);
 			var duration = trace.time - startTrace.time;
-			console.log("CALLTREE: " + indent + "} // (" + duration + "ms)");
+			console.log("CALLTREE: " + indent + "} // duration: " + duration + "ms");
 		}
 	};
 	while(stack.pop()) {
@@ -148,4 +201,6 @@ function print() {
 	console.log("Reset trace event queue");
 }
 
-
+function prettyNodeSource(node) {
+	return node.path + ":" + node.start.line + ":" + node.start.column + "~" + node.end.line + ":" + node.end.column;
+}
